042347c88b0153235e35ab217c9f6ed09a6a9089
aartisethi <aartisethi8194@gmail.com>
2020-06-26 12:49:33 +0530

    feat: responsiveness done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,8 +11,22 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('running on init');
-    particlesJS.load('particles-js', 'assets/data/particlesjs-config.json', function () {
-      console.log('callback - particles.js config loaded');
-    });
+    particlesJS.load(
+      'particles-js',
+      'assets/data/particlesjs-config.json',
+      function () {
+        console.log('callback - particles.js config loaded');
+      }
+    );
+    console.log('screen width', screen.width);
+    if (screen.width <= 600) {
+      const navbar = document.querySelector('.navbar');
+      document.querySelector('.nav-btn').addEventListener('click', () => {
+        navbar.classList.toggle('show');
+      });
+      navbar.addEventListener('click', () => {
+        navbar.classList.toggle('show');
+      });
+    }
   }
 }
